87f8c298c5181adfe4760926078da6ca7fffdd0e
Sebastian Kim <sydneyitguy@gmail.com>
2018-03-07 17:49:16 +0900

    Fix routes matches author name with dot included

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get '/posts/exists'
   resources :posts, only: [:index, :create] do
     collection do
-      get '/@:author/:permlink', to: 'posts#show'
-      put '/@:author/:permlink', to: 'posts#update'
-      delete '/@:author/:permlink', to: 'posts#destroy'
-      patch 'refresh/@:author/:permlink', to: 'posts#refresh'
+      get '/@:author/:permlink', to: 'posts#show', constraints: { author: /[^\/]+/ } # override, otherwise it cannot include dots
+      put '/@:author/:permlink', to: 'posts#update', constraints: { author: /[^\/]+/ }
+      delete '/@:author/:permlink', to: 'posts#destroy', constraints: { author: /[^\/]+/ }
+      patch 'refresh/@:author/:permlink', to: 'posts#refresh', constraints: { author: /[^\/]+/ }
     end
   end
 
