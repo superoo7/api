9a31e370d4e479d5eda509178f8abb03d470aa8e
Sebastian Kim <sydneyitguy@gmail.com>
2018-04-04 00:00:15 +0900

    Missing DB migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305031656) do
+ActiveRecord::Schema.define(version: 20180403070440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20180305031656) do
     t.string "encrypted_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "session_count", default: 0
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
