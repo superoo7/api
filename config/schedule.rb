18bf2b6ce08e848c353450ef387537bb4002870e
Sebastian Kim <sydneyitguy@gmail.com>
2018-05-05 19:39:16 +0900

    Fix sync & voting bot schedule properly

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,11 +5,10 @@ ROOT_DIR = '/srv/web/steemhunt/current'
 RAKE_PATH = '/home/updatebot/.rbenv/shims/bundle exec rake'
 
 every :day, at: '12:01am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} voting_bot"
-end
-
-every :day, at: '12:30am' do
-  command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts"
+  command "cd #{ROOT_DIR};" +
+    "RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts && " +
+    "RAILS_ENV=#{environment} #{RAKE_PATH} voting_bot && " +
+    "RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts"
 end
 
 every :day, at: '5:00am' do
