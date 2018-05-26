440e68c4c2bc4c23d532132f8ce902bf0d3dfbab
Sebastian Kim <sydneyitguy@gmail.com>
2018-05-26 23:02:26 +0900

    #23 - Fix migration

--- db/migrate/20180524161336_add_verified_by_column.rb
+++ db/migrate/20180524161336_add_verified_by_column.rb
@@ -2,7 +2,8 @@ class AddVerifiedByColumn < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :verified_by, :string, default: nil
 
-    Post.where('created_at < ?', Date.today).update_all(is_verified: true, verified_by: 'steemhunt')
-    Post.where('created_at >= ?', Date.today).where(is_verified: true).update_all(verified_by: 'project7')
+    today = Time.zone.today.to_time
+    Post.where('created_at < ?', today).update_all(is_verified: true, verified_by: 'steemhunt')
+    Post.where('created_at >= ?', today).where(is_verified: true).update_all(verified_by: 'project7')
   end
 end
