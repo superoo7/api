07611792aa40c9f2b05e32c9ab9fe1ae8e65d0f5
Sebastian Kim <sydneyitguy@gmail.com>
2018-05-26 22:26:31 +0900

    #23 - Old data migration

--- db/migrate/20180524161336_add_verified_by_column.rb
+++ db/migrate/20180524161336_add_verified_by_column.rb
@@ -1,5 +1,8 @@
 class AddVerifiedByColumn < ActiveRecord::Migration[5.2]
   def change
     add_column :posts, :verified_by, :string, default: nil
+
+    Post.where('created_at < ?', Date.today).update_all(is_verified: true, verified_by: 'steemhunt')
+    Post.where('created_at >= ?', Date.today).where(is_verified: true).update_all(verified_by: 'project7')
   end
 end
