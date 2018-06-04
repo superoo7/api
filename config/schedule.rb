b21a1a16992180a4cbdcbb20e4057460f524dd9c
Sebastian Kim <sydneyitguy@gmail.com>
2018-06-04 15:33:56 +0900

    Bot starts from 12:30

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ set :output, '/srv/web/steemhunt/shared/log/cron.log'
 ROOT_DIR = '/srv/web/steemhunt/current'
 RAKE_PATH = '/home/updatebot/.rbenv/shims/bundle exec rake'
 
-every :day, at: '12:01am' do
+every :day, at: '12:30am' do
   command "cd #{ROOT_DIR};RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] voting_bot && " +
     "RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[1] && RAILS_ENV=#{environment} #{RAKE_PATH} sync_posts[8]"
 end
